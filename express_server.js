4785ef4cf1eb687ecdfae3ebc308cf44f25cf539
Adam Thorne <ahthornev2@gmail.com>
2020-11-09 23:50:08 +0000

    created page template for /urls/:shortURL to render the information about a single URL

--- express_server.js
+++ express_server.js
@@ -11,7 +11,13 @@ const urlDatabase = {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  console.log(urlDatabase);
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {

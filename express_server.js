459c7ef69f5575521916827adc5a5017568e205a
Adam Thorne <ahthornev2@gmail.com>
2020-11-10 03:53:20 +0000

    added functionality to generate new random shortURLs for each POST request and update the URL database with the new key-value pair

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const generateRandomString = () => {
-  return Math.random().toString(36).substr(2, 5);
+  return (Math.random() + 1).toString(36).substr(2, 6);
 };
 
 const urlDatabase = {
@@ -16,17 +16,8 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
@@ -43,9 +34,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL; // save the shortURL-longURL pair to the database when it recieves a post request
+  res.redirect(`/urls/${newShortURL}`); // redirect to new shortURL, created by random string generator
 });
 
 app.listen(PORT, () => {

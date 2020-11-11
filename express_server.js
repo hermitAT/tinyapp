0c2b028f7791cbe71fd60ad503e1298323a1968e
Adam Thorne <ahthornev2@gmail.com>
2020-11-11 01:08:00 +0000

    functionality added to store a user-given ID (username) within a cookie, and clear that cookie when the user logs out of the site

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const generateRandomString = () => {
   return (Math.random() + 1).toString(36).substr(2, 6);
 };
@@ -13,32 +19,54 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+// ^^ above code is middleware, requirements, or functions/constants set for use within the app
 
+// create a .json file containing our key-value pairs within the urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// render the template for our urls homepage, using the urlDatabase
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
+// render the form to add a new short-longURL value pair to our database
 app.get("/urls/new", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
   res.render("urls_new");
 });
 
-// SHOW USER THE SHORTURL, LONGURL
+// show user the data for their short/longURL value pair, also contains form to edit the data or redirect to the original longURL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
-// REDIRECT USER TO LONGURL FROM THE SHORTURL ON THIS PAGE
+// redirect user to the longURL stored within the shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+// handle a post to /login the user
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+// handle a post to /logout the user
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 // ADD NEW URLs
@@ -50,21 +78,19 @@ app.post("/urls", (req, res) => {
 
 // EDIT URLS
 app.post("/urls/:shortURL", (req, res) => {
-  
   urlDatabase[req.params.shortURL] = req.body.longURL;
-
   res.redirect("/urls");
 });
 
 
 // DELETE URLs
 app.post("/urls/:shortURL/delete", (req, res) => {
-  
   delete urlDatabase[req.params.shortURL];
-
   res.redirect("/urls");
 });
 
+
+// app is listening...
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
